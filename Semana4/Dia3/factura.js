6ee347c3edf269e84e1c8634837ae3be97834825
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-06-19 11:36:33 -0500

    Solucion de la Factura de 2formas

--- Semana4/Dia3/factura.js
+++ Semana4/Dia3/factura.js
@@ -43,20 +43,35 @@ function Factura(){
             this.subtotal = +((this.total - (this.total*0.18)).toFixed(2));
             this.igv = +((this.total - this.subtotal).toFixed(2));
         },
+        // FORMA 1 => Con el constructor de detalles dentro de la Factura
         crearDetalle: function(_id, _cant, _descripcion, _punitario){
             //que retorne un objeto de tipo detalle
-            var objDetalle = {
-                id:_id,
-                cant:_cant,
-                descripcion:_descripcion,
-                punitario:_punitario,
-                subtotal:_punitario * _cant,
-            };
-            return objDetalle;
-        }
+                var objDetalle = {
+                    id:_id,
+                    cant:_cant,
+                    descripcion:_descripcion,
+                    punitario:_punitario,
+                    subtotal:_punitario * _cant,
+                };
+                return objDetalle;
+            }
+        
     };
     return objFactura;
 }
+
+// FORMA 2 => Con el constructor del Detalle independiente
+// function Detalle(_id, _cant, _descripcion, _punitario){
+//     //que retorne un objeto de tipo detalle
+//     var objDetalle = {
+//         id:_id,
+//         cant:_cant,
+//         descripcion:_descripcion,
+//         punitario:_punitario,
+//         subtotal:_punitario * _cant,
+//     };
+//     return objDetalle;
+// }
 
 var miFactura = Factura();
 
@@ -70,9 +85,18 @@ var miFactura = Factura();
 // var miDetalle1 = miFactura.crearDetalle(1,10,"Chelas",6);
 // var miDetalle2 = miFactura.crearDetalle(2,15,"Chizitos",0.5);
 
+// llamada de la FORMA 2 => USANDO EL CONSTRUCTOR INDEPENDIENTE
+// miFactura.agregarDetalle(Detalle(1,10,"Chelas",6));
+// miFactura.agregarDetalle(Detalle(2,15,"Chizitos",0.5));
+
+// llamada de la FORMA 1 => USANDO EL CONSTRUCTOR DENTRO DEL OBJETO FACTURA
 miFactura.agregarDetalle(miFactura.crearDetalle(1,10,"Chelas",6));
 miFactura.agregarDetalle(miFactura.crearDetalle(2,15,"Chizitos",0.5));
+
 miFactura.eliminarDetalle(1);
+
+
+prompt
 
 console.log(miFactura);
 
