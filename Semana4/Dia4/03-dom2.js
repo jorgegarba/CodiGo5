7ff0b1dbe55336d44fed67d52f9f1b2c19042a85
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-06-20 13:08:55 -0500

    Dom2

---
+++ Semana4/Dia4/03-dom2.js
@@ -0,0 +1,24 @@
+var parrafo1= document.getElementById("parrafo1");
+var parrafo2= document.getElementById("parrafo2");
+
+// innerHTML => devuelve o settea el contenido de un 
+// elemento HTML, es decir, lo que contiene su etiqueta
+console.log(parrafo1.innerHTML);
+
+parrafo2.innerHTML = "Nuevo contenido";
+parrafo1.innerHTML = "<i>El nuevo contenido</i>";
+
+var libros = ["Harry Potter y la piedra filosofal",
+              "Ensayo sobre la ceguera",
+              "Platero y Yo"];
+
+var resultado = "<ul>";
+
+for (let i = 0; i < libros.length; i++) {
+    resultado = resultado + "<li>";
+    resultado = resultado + `${libros[i]}`;
+    resultado = resultado + "</li>";
+}
+
+resultado = resultado + "</ul>";
+parrafo2.innerHTML = resultado;
