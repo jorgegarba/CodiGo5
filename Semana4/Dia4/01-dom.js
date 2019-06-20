cde474b2cb072ccde83a6ee1a19445d80ed102c0
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-06-20 11:25:26 -0500

    Tarea Dom

--- Semana4/Dia4/01-dom.js
+++ Semana4/Dia4/01-dom.js
@@ -1,4 +1,4 @@
-var div = document.getElementById("miDiv");
+var div = document.getElementById('miDiv');
 // ancho de un elemento
 console.log(div.clientWidth);
 // alto de un elemento
@@ -7,6 +7,13 @@ console.log(div.clientHeight);
 console.log(div.offsetWidth);
 // alto de un elemento incluido sus bordes
 console.log(div.offsetHeight);
+// cantidad de pixeles que un elemento se ha
+// desplazado desde la izquierda de su contenedor
+console.log(div.offsetLeft);
+// cantidad de pixeles que un elemento se ha
+// desplazado desde la parte superior
+// de su contenedor
+console.log(div.offsetTop);
 // posicion del elemento con referencia al top
 console.log(div.scrollTop);
 // posicion del elemento con referencia a left
@@ -18,5 +25,28 @@ var boton = document.getElementById("boton");
 boton.addEventListener("click",function(){
     div.style.height = `${div.clientHeight+2}px`;
     div.style.position = "relative";
-    div.style.left = "30px";
+    div.style.left = `${div.offsetLeft+2}px`;
+});
+// className => declarar u obtener la clase del
+// elemento
+var btnCambiarClase = document.getElementById("cambiarClase");
+btnCambiarClase.addEventListener("click",function(){
+    if(div.className == "nuevaClase"){
+        div.className = "miDiv";
+    }else{
+        div.className = "nuevaClase";
+    }
+});
+//classList => devuelve un array con las clases
+// que tiene un elemento
+var multiclase = document.querySelector("#multiclase");
+// classList.add("nombreDeLaClaseAAgregar")
+var agregarClase = document.getElementById("agregarClase");
+agregarClase.addEventListener("click",function(){
+    multiclase.classList.add("rotar");
+});
+// classList.remove("nombreDeLaClaseAEliminar")
+var quitarClase = document.getElementById("quitarClase");
+quitarClase.addEventListener("click",function(){
+    multiclase.classList.remove("rotar");
 });
