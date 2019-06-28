3e0d5d1a648603b2421cd0ace4329385f0943ab5
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-06-28 13:07:42 -0500

    Ejercicios Producctos version 1 y version 2

---
+++ Semana5/Dia5/Bootstrap/14-ejercicioProductos.js
@@ -0,0 +1,95 @@
+window.onload = function(){
+
+    var main = document.getElementById("contenedor");
+    var modalTitulo = document.getElementById("modalTitulo");    
+    var modalImagen = document.getElementById("modalImagen");
+    var modalDescripcion = document.getElementById("modalDescripcion");    
+
+    var productos = [{
+        id:10,
+        nombre:"Samsung Galaxy S8",
+        descripcion:"Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.",
+        imagen:"http://placehold.it/300x400/",
+    },{
+        id:20,
+        nombre:"Samsung Galaxy S9",
+        descripcion:"Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.",
+        imagen:"http://placehold.it/300x400/",
+    },
+    {
+        id:30,
+        nombre:"Samsung Galaxy S10",
+        descripcion:"Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.",
+        imagen:"http://placehold.it/300x400/",
+    },
+    {
+        id:40,
+        nombre:"Samsung Galaxy S11",
+        descripcion:"Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.",
+        imagen:"http://placehold.it/300x400/",
+    }];
+
+    function buscarProductById(idBuscado){
+        for (let i = 0; i < productos.length; i++) {
+            if(productos[i].id == idBuscado){
+                return productos[i];
+            }
+        }
+        return null;
+    }
+
+
+    var row = document.createElement("div");
+    row.className = "row";
+    main.appendChild(row);
+    var divProducto = ""
+    for(let i = 0; i < productos.length; i++){
+         divProducto = divProducto +  `<div class="col-md-4 mb-2">
+                                            <div class="card">
+                                                <img src="${productos[i].imagen}" alt="" class="card-img-top">
+                                                <div class="card-body">
+                                                    <h4 class="card-title">${productos[i].nombre}</h4>
+                                                    <p class="card-text">
+                                                        ${productos[i].descripcion}
+                                                    </p>
+                                                    <button class="btn btn-primary clickable"
+                                                            idproducto = "${productos[i].id}"
+                                                            type="button">
+                                                        Ver Producto
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>`; 
+                                               
+    }
+    row.innerHTML = divProducto;
+
+    var botones = document.querySelectorAll(".clickable");
+    for (let i = 0; i < botones.length; i++) {
+        // var temp = document.getElementById(`btnAbrir${i}`);
+        // console.log(temp);
+        botones[i].onclick = function(){
+            // reiniciar los campos del modal
+            modalTitulo.innerHTML = "------------------";
+            modalDescripcion.innerHTML = "-----------------------";
+            modalImagen.setAttribute("src","https://www.oei.es/historico/divulgacioncientifica/IMG/arton5169.jpg?1560240621");
+
+            var idProducto = this.getAttribute("idProducto");
+            var productoEncontrado = buscarProductById(idProducto);
+
+            if(!productoEncontrado){
+                location.reload();                
+            }else{
+                $("#miModal").modal("show");
+                modalTitulo.innerHTML = productoEncontrado.nombre;
+                modalDescripcion.innerHTML = productoEncontrado.descripcion;
+                modalImagen.setAttribute("src",productoEncontrado.imagen);
+            }
+
+            
+        };
+    }
+}
+
+
+
---
+++ Semana5/Dia5/Bootstrap/15-ejercicioProductosV2.js
@@ -0,0 +1,90 @@
+window.onload = function(){
+
+    var main = document.getElementById("contenedor");
+    var modalTitulo = document.getElementById("modalTitulo");    
+    var modalImagen = document.getElementById("modalImagen");
+    var modalDescripcion = document.getElementById("modalDescripcion");    
+
+    var productos = [{
+        id:10,
+        nombre:"Samsung Galaxy S8",
+        descripcion:"Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.",
+        imagen:"http://placehold.it/300x400/",
+    },{
+        id:20,
+        nombre:"Samsung Galaxy S9",
+        descripcion:"Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.",
+        imagen:"http://placehold.it/300x400/",
+    },
+    {
+        id:30,
+        nombre:"Samsung Galaxy S10",
+        descripcion:"Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.",
+        imagen:"http://placehold.it/300x400/",
+    },
+    {
+        id:40,
+        nombre:"Samsung Galaxy S11",
+        descripcion:"Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.",
+        imagen:"http://placehold.it/300x400/",
+    }];
+
+    var row = document.createElement("div");
+    row.className = "row";
+    main.appendChild(row);
+    
+    for(let i = 0; i < productos.length; i++){
+
+        var colmd4 = document.createElement("div");
+        colmd4.className= "col-md-4 mb-2";
+
+        var card = document.createElement("div");
+        card.className= "card";
+        
+        colmd4.appendChild(card);
+
+        var img = document.createElement("img");
+        img.className = "card-img-top";
+        img.setAttribute("src",`${productos[i].imagen}`);
+
+        var body = document.createElement("div");
+        body.className = "card-body";
+
+        card.appendChild(img);
+        card.appendChild(body);
+
+        var h4 = document.createElement("h4");
+        h4.className = "card-title";
+        h4.innerHTML = `${productos[i].nombre}`;
+
+        var p = document.createElement("p");
+        p.className ="card-text";
+        p.innerHTML =`${productos[i].descripcion}`;
+
+        var boton = document.createElement("button");
+        boton.className="btn btn-primary";
+        boton.setAttribute("type","button");
+        boton.innerHTML="Ver Producto";
+
+        boton.onclick = function(){
+
+            var idProducto = productos[i].id;            
+            $("#miModal").modal("show");
+            modalTitulo.innerHTML = productos[i].nombre;
+            modalDescripcion.innerHTML = productos[i].descripcion;
+            modalImagen.setAttribute("src",productos[i].imagen);
+            
+        }
+
+        body.appendChild(h4);
+        body.appendChild(p);
+        body.appendChild(boton);
+
+        row.appendChild(colmd4);
+                                               
+    }
+
+}
+
+
+
