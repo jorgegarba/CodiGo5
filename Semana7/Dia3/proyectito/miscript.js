5dc487640c4df0fc724861cb0f446336bfc137e4
Osmar Montesinos <osmarmontes50@gmail.com>
2019-07-10 12:49:07 -0500

    dt primer funcionamiento

---
+++ Semana7/Dia3/proyectito/miscript.js
@@ -0,0 +1,20 @@
+//document ready se ejecutará cuando ya este listo el document
+$(document).ready(function(){
+    $("#listaProductos").DataTable({
+        "ajax": {
+            type:"GET",
+            url:"http://5d258f57d924540014931687.mockapi.io/Productos",
+            timeout:1500,
+            //dataSrc hará que maneje toda la información a partir de un arreglo
+            dataSrc:""
+        },
+        //columns tiene que tener asignado un arreglo con [{data:nombre_del_campo},...]
+        "columns":[
+            {data:"prod_id"},
+            {data:"prod_nom"},
+            {data:"prod_desc"},
+            {data:"prod_price"},
+            {data:"prod_pic"}
+        ]
+    })
+})
