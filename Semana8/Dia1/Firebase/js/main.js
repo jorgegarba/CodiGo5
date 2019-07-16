b5b5a5dc9dad6089dec936bf396c7d22bae8e545
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-07-16 12:19:58 -0500

    Modal para crear platos

--- Semana8/Dia1/Firebase/js/main.js
+++ Semana8/Dia1/Firebase/js/main.js
@@ -7,6 +7,7 @@ import { firebaseConfig } from "./variables.js";
 window.onload = () => {
 
 
+
     // Iniciando la configuración de firebase con nuestra aplicación
     firebase.initializeApp(firebaseConfig);
 
@@ -16,28 +17,70 @@ window.onload = () => {
 
     }
     if (location.href.indexOf("platos") >= 0) {
+        // configurando click al boton de agregar plato
+        // para aparecer modal
+        $("#btnCrearPlato").click(()=>{
+            $("#modalCrearPlato").modal("show")
+        });
         // estamos en platos.html
         $.notify("estamos en platos.html", "info");
-        // Inicializar la base de datos en tiempo real
-        // creando una referencia al nodo principal
-        // database() => funcion disponible con el script de database firebase
-        let refPlatos = firebase.database().ref("platos");
-        refPlatos.on("value", dataSnapshot => {
-            // FORMA 1 PARA CONSUMIR LOS DATOS de dataSnapshot
-            // let data = dataSnapshot.val();
-            // for (const clave in data) {
-            //     console.log(clave);
-            //     console.log(data[clave].calorias);
-            //     console.log(data[clave].origen);
-            // }
-            // FORMA 2 PARA CONSUMIR LOS DATOS de dataSnapshot
+
+        let renderizarPlatos = (dataSnapshot) => {
+            /**
+             * 1. limpiar el conteneder de cards (main)
+             */
+            $("main").html("");
+            /**
+             * 2. crear una variable string que guardara
+             * la estructura del contenido de main (cards)
+             */
+            let contenido = "";
+            contenido += `<div class="card-columns">`;
             dataSnapshot.forEach(plato => {
-                console.log(plato.key);
-                console.log(plato.val().calorias);
-                console.log(plato.val().origen);
+                contenido += `<div class="card">
+                                <div class="card-body">
+                                <h5 class="card-title">${plato.val().nombre}</h5>
+                                <p class="card-text">${plato.val().descripcion}</p>
+                                </div>
+                            </div>`;
             });
-            
-        })
+            contenido += `</div>`;
+            $("main").html(contenido);
+        }
+
+        let getPlatos = () => {
+            $("main").html(`<div class="text-center">
+                                <div class="spinner-border" role="status">
+                                <span class="sr-only">Loading...</span>
+                                </div>
+                            </div>`);
+            // Inicializar la base de datos en tiempo real
+            // creando una referencia al nodo principal
+            // database() => funcion disponible con el script de database firebase
+            let refPlatos = firebase.database().ref("platos");
+            refPlatos.on("value", dataSnapshot => {
+                // FORMA 1 PARA CONSUMIR LOS DATOS de dataSnapshot
+                // let data = dataSnapshot.val();
+                // for (const clave in data) {
+                //     console.log(clave);
+                //     console.log(data[clave].calorias);
+                //     console.log(data[clave].origen);
+                // }
+                // FORMA 2 PARA CONSUMIR LOS DATOS de dataSnapshot
+                // dataSnapshot.forEach(plato => {
+                //     console.log(plato.key);
+                //     console.log(plato.val().calorias);
+                //     console.log(plato.val().origen);
+                // });
+                renderizarPlatos(dataSnapshot);
+            });
+
+        }
+
+
+
+        getPlatos();
+
     }
 }
 
