afddba77e7dd238bcf786810b5314105bd583d9a
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-08-10 10:37:12 -0500

    Modulo Intranet

--- Semana11/apptiempo/src/app/app-routing.module.ts
+++ Semana11/apptiempo/src/app/app-routing.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const MIS_RUTAS: Routes = [
   {
-      path: '',
-      component: HomeComponent
+    path: '',
+    component: HomeComponent
   },
+  {
+    path: 'intranet'
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
 ];
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(MIS_RUTAS)
-  ],
-  exports:[
-    RouterModule
-  ]
+  imports: [RouterModule.forRoot(MIS_RUTAS)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Semana11/apptiempo/src/app/intranet/intranet.module.ts
+++ Semana11/apptiempo/src/app/intranet/intranet.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule
   ]
