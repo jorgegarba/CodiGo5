8da80aec776a62cb4b55d8ccf645a6b019c7f95b
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-09-02 11:11:23 -0500

    STATES

--- Semana14/02-statecomponents/src/App.js
+++ Semana14/02-statecomponents/src/App.js
@@ -4,15 +4,31 @@ import Navbar from './components/Navbar';
 
 class App extends Component {
 
-  productos = [{ id: 1, nombre: "Producto 1", precio: 100 },
-  { id: 2, nombre: "Producto 2", precio: 200 },
-  { id: 3, nombre: "Producto 3", precio: 300 }];
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: [
+        { id: 1, nombre: "Producto 1", precio: 100 },
+        { id: 2, nombre: "Producto 2", precio: 200 },
+        { id: 3, nombre: "Producto 3", precio: 300 }
+      ],
+      producto: {}
+    }
+  }
+
+  seleccionarProducto = (producto) => {
+    this.setState({
+      producto: producto
+    });
+  }
+
 
   render() {
     return (
       <div>
         <Navbar marca={"CodiGo"} />
-        <Productos productos={this.productos} />
+        <Productos productos={this.state.productos}
+          seleccionarProducto={this.seleccionarProducto} />
       </div>
     )
   }
--- Semana14/02-statecomponents/src/components/Productos.js
+++ Semana14/02-statecomponents/src/components/Productos.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-
 export default class Productos extends Component {
 
     constructor(props) {
         super(props);
         console.log("Productos constructor");
         this.state = {
+            productos: props.productos,
             producto: {},
-        }
+        };
     }
 
     seleccionarProducto = (prod) => {
@@ -17,10 +17,32 @@ export default class Productos extends Component {
         })
     }
 
+    componentDidMount() {
+        console.log("Producto componentDidMount");
+        // Funcion que se ejecuta luego del render cuando todo DOM ya ha sido cargado
+        // Esta función sólo se ejecuta una vez
+        // Esta funcion puede modificar el state
+        // Esta funcion se usa generalmente, para traer data mediante API's.
+        // Esta funcion tambien es usada para settear objetos JQUERY,
+        // EJM: DatePickers, modales, datatables, etc
+    }
+
+    generarAleatorio = e => {
+        // generando un producto aleatorio
+        let productoTmp = {
+            id: Math.floor(Math.random() * (50 - 5)) + 5,
+            nombre: 'Producto aleatorio',
+            precio: Math.random() * (10000 - 1000) + 1000,
+        };
+        this.setState({
+            productos: [...this.state.productos, productoTmp]
+        });
+    }
+
     render() {
         console.log("Productos render");
 
-        let { productos } = this.props;
+        let { productos } = this.state;
 
         return (
             <div>
@@ -43,7 +65,7 @@ export default class Productos extends Component {
                                         <td>{prod.precio}</td>
                                         <td>
                                             <button onClick={(e) => {
-                                                this.seleccionarProducto(prod);
+                                                this.props.seleccionarProducto(prod);
                                             }}>
                                                 Seleccionar
                                             </button>
@@ -59,6 +81,10 @@ export default class Productos extends Component {
                         (<p>Producto Seleccionado: {this.state.producto.nombre}</p>) :
                         (<p>No has seleccionado ningun producto</p>)
                 }
+
+                <button onClick={this.generarAleatorio}>
+                    Generar Producto aleatorio
+                </button>
 
             </div>
         )
